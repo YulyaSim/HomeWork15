8310c63ac4297d583a8f721c046e5eabbcce1b4d
YulyaSim <152449597+YulyaSim@users.noreply.github.com>
2024-01-30 08:23:46 +1100

    1

---
+++ PracticalWork15/PracticalWork15.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+void FindOddNumbers(int Limit, bool IsOdd) 
+{
+    for (int i = int(IsOdd) + 1; i <= Limit; i += 2)
+        std::cout << i << "\n";
+}
+
+int main()
+{
+    int num;
+    bool Odd;
+    cout << "Are you looking for? Odd(y/n):";
+    string yes; 
+    cin >> yes;
+    Odd = (yes == "y");
+    cout << "Enter the limit:";
+    cin >> num;
+    FindOddNumbers(num,Odd);
+   
+   
+    
+}
